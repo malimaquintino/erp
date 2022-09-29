ba7a66dbbe7861e5bf7418a101efd04378d2a92d
Matheus Quintino <malimaquintino@gmail.com>
2022-09-29 20:59:45 -0300

    feign fix

--- customer/src/main/java/com/malimaquintino/erp/customer/requests/ProductRequest.java
+++ customer/src/main/java/com/malimaquintino/erp/customer/requests/ProductRequest.java
@@ -1,14 +1,15 @@
 package com.malimaquintino.erp.customer.requests;
 
+import com.malimaquintino.erp.commonmslib.dto.common.CommonResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "product")
+@FeignClient(value = "catalog-ms")
 public interface ProductRequest {
 
     @GetMapping(path = "/v1/product/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<?> findProductById(@PathVariable long id);
+    ResponseEntity<CommonResponse> findProductById(@PathVariable long id);
 }
