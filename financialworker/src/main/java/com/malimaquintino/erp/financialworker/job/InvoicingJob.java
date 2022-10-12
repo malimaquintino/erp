d36d3e305a2e28545efac5e48c7bc3e179cdc02b
Matheus Quintino <malimaquintino@gmail.com>
2022-10-12 16:47:57 -0300

    add incremeter spring batch

--- financialworker/src/main/java/com/malimaquintino/erp/financialworker/job/InvoicingJob.java
+++ financialworker/src/main/java/com/malimaquintino/erp/financialworker/job/InvoicingJob.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,6 +25,7 @@ public class InvoicingJob {
         return jobBuilderFactory.get("createInvoice")
                 .start(testOne)
                 .next(testTwo)
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 }
